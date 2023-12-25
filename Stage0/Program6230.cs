9c5ecaf016b468e1092d2ee2854b87d33ddcb090
pwalles <pwalles@g.jct.ac.il>
2023-12-25 15:53:02 +0200

    Initial Code + Welcome functions"

---
+++ Stage0/Program6230.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome6230();
+        Welcome1640();
+        Console.ReadKey();
+    }
+
+    private static void Welcome6230()
+    {
+        Console.WriteLine("Enter your nuame: ");
+        String name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first program", name);
+    }
+
+     static partial void Welcome1640();
+}
