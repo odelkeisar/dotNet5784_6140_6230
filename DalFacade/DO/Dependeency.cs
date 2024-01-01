d3e98bcc80a6e94fb179a51a48b9d06bacf56814
keisarodel@gmail.com <keisarodel@gmail.com>
2024-01-01 21:08:32 +0200

    עדכון הערות

--- DalFacade/DO/Dependeency.cs
+++ DalFacade/DO/Dependeency.cs
@@ -1,5 +1,10 @@
 ﻿namespace DO;
-
+/// <summary>
+/// Indicates the dependency between tasks, i.e. which task can only be executed after the completion of a previous task.
+/// </summary>
+/// <param name="Id">Unique ID number (automatic runner number)</param>
+/// <param name="DependentTask">ID number of pending task</param>
+/// <param name="DependsOnTask">Previous assignment ID number</param>
 public record Dependeency
 (
     int Id,
--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,9 +1,17 @@
 ﻿namespace DO;
+/// <summary>
+/// Engineer
+/// </summary>
+/// <param name="Id">Unique ID number</param>
+/// <param name="Email">Email</param>
+/// <param name="Cost">cost per hour</param>
+/// <param name="Name">Engineer's name (full name)</param>
+/// <param name="Level">Engineer level</param>
 public record Engineer
 (
-    int It,
+    int Id,
     string? Email = null,
     double? Cost = null,
     string? Name = null,
-    DO.EngineerExperience Level
+    DO.EngineerExperience? Level=null
  );
