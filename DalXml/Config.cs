8c2c6df2d714aee0c1492d63a497c4f54cea4c79
keisarodel@gmail.com <keisarodel@gmail.com>
2024-01-15 17:51:13 +0200

    פרק 3 שלב1-3

---
+++ DalXml/Config.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+    internal static int NextTask1Id { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTask1Id"); }
+    internal static int NextDependeencyId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDependeencyId"); }
+}
+
