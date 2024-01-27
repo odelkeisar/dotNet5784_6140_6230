898b2edcc3ad1b9d5c54e1e53321db3fd2b31e53
pwalles <pwalles@g.jct.ac.il>
2024-01-27 21:49:27 +0200

    הוספת שגיאות

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿namespace DO;
+[Serializable]
+public class BlDoesNotExistException : Exception //אובייקט לא קיים
+{
+    public BlDoesNotExistException(string? message) : base(message) { }
+    public BlDoesNotExistException(string message, Exception innerException)
+                : base(message, innerException) { }
+}
+
+
+[Serializable]
+public class BlNullPropertyException : Exception //שגיאה עם עובדים עם ערך null 
+{
+    public BlNullPropertyException(string? message) : base(message) { }
+}
+
+[Serializable]
+public class BlAlreadyExistsException : Exception //האובייקט כבר קיים
+{
+    public BlAlreadyExistsException(string? message) : base(message) { }
+    public BlAlreadyExistsException(string message, Exception innerException)
+                : base(message, innerException) { }
+}
+
